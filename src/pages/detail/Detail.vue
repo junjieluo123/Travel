0452741b3eb543b2b145aee7782709ed4c6e86da
junjieluo123 <578715385@qq.com>
2020-07-12 16:08:15 +0800

    ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,13 +1,22 @@
 <template>
 	<div>
-		<detail-banner></detail-banner>
+		<detail-banner 
+			:sightName="sightName"
+			:bannerImg="bannerImg"
+			:bannerImgs="gallaryImgs"
+		>	
+		</detail-banner>
 		<detail-header></detail-header>
-		<detail-list :list="list"></detail-list>
+		<detail-list 
+			:list="list"
+		>	
+		</detail-list>
 		<div class="content"></div>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios'
 	import DetailBanner from './components/Banner'
 	import DetailHeader from './components/Header'
 	import DetailList from './components/List'
@@ -20,24 +29,34 @@
 		},
 		data () {
 			return {
-				list: [{
-					title: '成人票',
-					children:[{
-						title: '成人三馆联票',
-						children:[{
-							title: '成人三馆联票-某一连锁点销售'
-						}]
-					},{
-						title: '成人五馆联票'
-					}]
-				},{
-					title: '学生票'
-				},{
-					title: '儿童票'
-				},{
-					title: '特惠票'
-				}]
+				sightName: '',
+				bannerImg: '',
+				gallaryImgs: [],
+				list: []
 			}
+		},
+		methods: {
+			getDetailInfo () {
+				axios.get('/api/detail.json',{
+					params: {
+						id: this.$route.params.id
+					}
+				}).then(this.handleGetDataSucc)
+		},
+		 	handleGetDataSucc (res) {
+		 		res = res.data
+		 		if (res.ret && res.data) {
+		 			const data = res.data
+		 			this.sightName = data.sightName
+		 			this.bannerImg = data.bannerImg
+		 			this.gallaryImgs = data.gallaryImgs
+		 			this.list = data.categoryList
+		 			console.log(data)
+		 		}
+		 	}
+		 },
+		mounted () {
+			this.getDetailInfo()
 		}
 	}
 </script>
